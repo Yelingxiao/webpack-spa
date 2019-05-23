1d67f424f51192604e2feedc5873792ee7503b4c
yelingxiao <1003772257@qq.com>
2019-05-23 19:21:11 +0800

    30s-code

--- src/30s-code/array.js
+++ src/30s-code/array.js
@@ -5,12 +5,21 @@ const allEqual = arr => arr.every(val => val === arr[0])
 const any = (arr, fn = Boolean) => arr.some(fn)
 
 const arrayToCSV = (arr, delimiter = ',') => {
-  arr.map(v => {
+  arr
+    .map(v => {
       v.map(x => {
-          (isNaN(x) ? `"${x.replace(/"/g, '""')}"` : x)
-        }).join(delimiter)
-    }).join('\n')
+        isNaN(x) ? `"${x.replace(/"/g, '""')}"` : x
+      }).join(delimiter)
+    })
+    .join('\n')
 }
 
 const bifurcate = (arr, indices) => arr.filter((item, i) => indices[i])
-console.log(bifurcate([1, 2, 3, 4], [1,1,0,1]))
+bifurcate([1, 2, 3, 4], [1, 1, 0, 1])
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => {
+    return  arr.slice(i * size, i * size + size)
+  })
+let a = chunk([1, 2, 3, 4, 5], 2) // [[1,2],[3,4],[5]]
+console.log(a)
