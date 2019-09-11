81a20d5e97ce4f21a2cfa1ca07941725ba131bbe
yelingxiao <1003772257@qq.com>
2019-09-11 16:41:08 +0800

    findMedianSortedArrays

---
+++ src/leetcode/findMedianSortedArrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findMedianSortedArrays = function(nums1 = [], nums2 = []) {
+  const arr = [...nums1, ...nums2].sort((a, b) => a - b)
+  console.log(arr)
+  const { length } = arr
+  return length % 2 ? arr[~~(length / 2)] : (arr[length / 2] + arr[length / 2 - 1]) / 2
+}
+
+let a = findMedianSortedArrays([1, 3], [2])
+console.log(a)
